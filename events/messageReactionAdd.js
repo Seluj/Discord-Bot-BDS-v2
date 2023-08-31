b4a8118714066aa6022bec290cd122021a06dae4
jules <jules.ferlin@gmail.com>
2023-08-31 18:36:51 +0200

    modified and added log messages

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -4,7 +4,9 @@ const {log, checkName, checkRole} = require('../utils/utils');
 module.exports = {
   name: Events.MessageReactionAdd,
   async execute(reaction, user) {
-    log("Message Reaction added");
+
+    let logString = "";
+
     if (reaction.partial) {
       // If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
       try {
@@ -23,34 +25,50 @@ module.exports = {
     const {ligne_de_départ, logs} = require(`../serveur/channels/channels_${reaction.message.guild.id}.json`);
 
     let channel_logs = null;
-    if (logs === undefined) {
-      log("Aucun salon 'logs'", null);
-    } else {
+    if (logs !== undefined) {
       channel_logs = reaction.message.guild.channels.cache.get(logs);
     }
 
     if (reaction.message.channel.id === ligne_de_départ) {
+      logString += `${user.tag} a réagi au message de règles avec ${reaction.emoji}`;
+
       if (reaction.emoji.name === '👍') {
-        log(`${user.tag} a réagi au message de règles`, channel_logs);
+
+        //log(`${user.tag} a réagi au message de règles`, channel_logs);
         const {Attente_Cotisant, Membre_du_Bureau} = require(`../serveur/roles/role_${reaction.message.guild.id}.json`);
 
         // Mise en place des rôles
         if (Attente_Cotisant === undefined || Membre_du_Bureau === undefined) {
-          log("Aucun Role 'Attente Cotisant'", channel_logs);
+          logString = "Aucun Role 'Attente Cotisant'";
+          //log("Aucun Role 'Attente Cotisant'", channel_logs);
         } else {
-          let member = await reaction.message.guild.members.fetch(user.id);
-          if (checkRole(member, Membre_du_Bureau)) {
-            //await member.send("Salut, t'es un membre du bureau je crois !!\nArrête de jouer avec le bot et retourne à ton poste de " + member.displayName.split(' - ')[1]);
+          let member = await reaction.message.guild.members.fetch(user.id); // On récupère le membre pour avoir toutes ses informations
+          logString += ` (${member.displayName})`
+
+          // On regarde si la personne est un membre du bureau
+          if (checkRole(member, Membre_du_Bureau)) {  // Si oui, on envoie un petit message personalisé et on quitte la fonction
             await member.send("Hop hop, camarade du bureau en escapade botique !\nReviens en mode " + member.displayName.split(' - ')[1] + ", le bot est jaloux de ton attention !\n");
             await reaction.users.remove(member.id);
-          } else if (!checkName(member.displayName)) {
-            await member.send("Salut, je crois que tu n'as pas bien lu, renomme toi correctement Prénom Nom, sans ta promo et sans ton surnom !!");
-            await reaction.users.remove(member.id);
+            logString += " et est un membre du bureau.";
           } else {
-            await member.roles.add(Attente_Cotisant);
+
+            // On regarde si le pseudo est correcte
+            if (checkName(member.displayName)) {    // Si oui, on lui ajoute le rôle Attente_Cotisant
+              await member.roles.add(Attente_Cotisant);
+              logString += " et est Attente_Cotisant.";
+            } else {                                // Sinon, on envoie un message
+              await member.send("Salut, je crois que tu n'as pas bien lu, renomme toi correctement Prénom Nom, sans ta promo et sans ton surnom !!");
+              await reaction.users.remove(member.id);
+              logString += ` et n'est pas bien renommé.`;
+            }
           }
         }
+
+      } else {
+        await reaction.users.remove(user.id);
       }
     }
+
+    log(logString, channel_logs);
   },
 };
