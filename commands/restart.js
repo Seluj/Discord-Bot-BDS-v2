09af7f091da67e083371829bc5186a39dae57256
jules <jules.ferlin@gmail.com>
2023-08-31 18:39:09 +0200

    removed old stuff

--- commands/restart.js
+++ commands/restart.js
@@ -6,18 +6,17 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName('restart')
     .setDescription('Initialise le bot après un redémarrage')
-    .setDefaultMemberPermissions(0)
-    .addChannelOption(
-      option =>
-        option
-          .setName('first_channel')
-          .setDescription('Channel où se trouver le premier message')
-          .setRequired(true)
-    ),
+    .setDefaultMemberPermissions(0),
   async execute(interaction) {
-    let option_channel = interaction.options.getChannel('first_channel');
+
+    const {ligne_de_départ} = require(`../serveur/channels/channels_${interaction.guild.id}.json`);
+    if (ligne_de_départ === undefined) {
+      interaction.reply({content: "ligne_de_départ n'existe pas", ephemeral: true});
+      return;
+    }
+
     let message;
-    let channel = await interaction.guild.channels.cache.get(option_channel.id);
+    let channel = await interaction.guild.channels.cache.get(ligne_de_départ);
     let response = '';
 
     if (channel.isTextBased) {
